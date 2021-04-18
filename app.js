d542e388856d37201e58cf896ba879b1b2ce3112
fasiha <fasiha2555@gmail.com>
2021-04-18 09:28:59 +0500

    work accomplish==============

---
+++ app.js
@@ -0,0 +1,377 @@
+// Chapter# 1:
+//Q1. Write a script to greet your website visitor using JS alert box.
+//var greet="Hello World";
+//alert(greet);
+//Q2. Write a script to display following message on your web page.
+//var popup="Error! Please Enter a valid password.";
+//alert(popup);
+//Q3. Write a script to display following message on your web page: (Hint : Use line break).
+//var popup="Welcome to JS Land...\n Happy Coding!";
+//alert(popup);
+//Q4(a). Write a script to display following messages in sequence:
+//var popup1="Welcome to JS Land...";
+//alert(popup1);
+//Q4(b).
+//var popup2="Happy Coding!\n Prevent this page from creating additional dialogs";
+//alert(popup2);
+//Q5. Generate the following message through browser's developer console:
+//var popup="Hello.. I can run JS through my web browser's console";
+  //alert(popup);
+//Q6. Make use of alerts in your new/existing HTML and CSS project.
+//var Text1 ="My Email Is:"
+//var Email = "fasiha2555@gmail.com"
+//var MyEmail = Text1 + Email
+//alert(MyEmail);
+//Q7.
+//done in index.html 
+
+
+
+
+
+//Chapter#2:
+
+//Q1. Declare a veriable called username.
+//var username="Hareem";
+//alert(username);
+
+//Q2.
+//var myName="Ariba";
+//alert(myName);
+
+//Q3. part(a)
+//var titled="JS variable";
+//alert(titled);
+
+//Q3. part(b)
+//var assign="Hello World";
+//alert(assign);
+
+//Q3. part(c)
+//alert("Hello World");
+
+//Q4.
+
+//var Name="John Doe";
+//var Age="15 years old";
+//var Qualification="Certified Mobile Application Development";
+//alert(Name);
+//alert(Age);
+//alert(Qualification);
+
+//Q5.
+//var display="PIZZA\n\PIZZ\nPIZ\nPI\nP";
+//alert(display);
+
+//Q6.
+//var email = " My email addresss is ";
+//var emailaddress = " example@example.com ";
+//var c =  email + emailaddress ;
+//alert (c);
+
+
+//Q7.
+//var book ="A smarter way to learn JavaScript";
+//alert("I am trying to learn from the Book A smarter way to learn JavaScript");
+
+//Q8.
+//var script = "Yah! I can write HTML content through Javascript";
+//document.write(script);
+
+
+//Q9.
+//var browser = " ------------*****------------" + "<br>" + "END";
+//document.write(browser);
+
+
+//var popup = "------------*****------------\nEND";
+//alert(popup);
+
+
+
+
+
+
+
+//CHAPTER # 3:
+
+//Q1.
+//var age= 15;
+//alert("I am" + " " +  age + "years old"  );
+
+
+//Q2.
+//var n = 14 ;
+//alert("You have visited this site" + " " + n + "times" );
+
+
+//Q3.
+//var birthyear=1990;
+//document.write("My birth Year is " + " " + birthyear + "<br>" + "Data type of my declared variable is number");
+
+
+
+//Q4.
+//var name= "John Doe";
+//var product="Tshirt";
+//var quantity=5;
+//var message =" "  + name + "&nbsp" + "ordered " + " " + quantity + " "+ product + " (s) on XYZ clothing store";
+//document.write(message);
+
+
+//CHAPTER # 4
+
+//Q1.
+
+//var myName = "666,hiba,arwa";
+//document.write(myName);
+
+//Q2.
+
+//var a="letters";
+//var b="digits";
+//var c="underscore";
+//var d="dollarsign";
+//var message =" " +a + " <br>" + " " +b + " <br>" + " " +c+ " <br>" + " " +d;
+//document.write(message);
+
+//var legal="legalVariableNames:\nletters,digits,underscore,dollar signs,"
+//alert(legal);
+
+
+//var illegal="123test,.alwaz,"
+
+
+
+
+
+//Q3.
+//part(a):
+//var heading = "<h1> Rules for naming JS variables </h1>";
+//document.write(heading);
+
+//part(b):
+
+//var a ="Variable names can only contain , numbers , letters , $ and _ . For Example : $my_1stVariable" + "<br>"
+//+ "Variable must begin with a letter , $ or _ . For Example : $name , _name or name " + "<br>" + "Variable names are case sensitive" + "<br>"
+//+ "Variable names should not be JS Keywords"  ;
+
+//document.write(a);
+
+
+
+
+
+//CHAPTER # 5
+//MATHS EXPRESSIONS :
+
+//Q1.
+
+//var a = 3;
+//var b = 5;
+//var c = a+b;
+//document.write("Sum of 3 and 5 is ");
+
+//document.write(c);
+
+
+//Q2.subtraction :
+
+//var a = 3;
+//var b = 5;
+//var c = a-b;
+
+
+//document.write("Subtraction of 3 and 5 is ");
+//document.write(c);
+
+//Q2.Multiplication:
+
+//var a = 3;
+//var b = 5;
+//var c = a*b;
+
+
+//document.write("Multiplication of 3 and 5 is ");
+//document.write(c);
+
+
+//Q2.Division:
+
+//var a = 3;
+//var b = 5;
+//var c = a/b;
+
+
+//document.write("Division of 3 and 5 is ");
+//document.write(c);
+
+
+//Q2.Modulus:
+
+//var a = 3;
+//var b = 5;
+//var c = a%b;
+
+
+//document.write("Modulus of 3 and 5 is ");
+//document.write(c);
+
+
+//Q3.
+
+//var a = 5;
+//document.write("Initial value : ");
+//document.write(a);
+//var a = 5;
+//var b = ++a ;
+//document.write("Value after increment is:");
+
+//document.write(b);
+//var a = 5;
+//var b = ++a ;
+//var c = b + 7;
+//document.write("Value after addition is:");
+//document.write(c);
+//var a = 5;
+//var b = ++a ;
+//var c = b + 7;
+//var d = --c ;
+//document.write("Value after decrement is:");
+
+//document.write(d);
+//var a = 5;
+//var b = ++a ;
+//var c = b + 7;
+//var d = --c ;
+//var e = d%3;
+//document.write("The remainder is :");
+//document.write(e);
+
+
+
+
+
+//Q4.
+//var z= "600" ;
+//var x= 5 ;
+//var y= z*x;
+//document.write("Total cost to buy 5 tickets to a movie is " );
+//document.write(y);
+
+
+//Q5.
+
+//var table = prompt("Please enter any number")
+//for (var a=1; a<=10; a++);
+//{
+//document.write(table + "*" + a + "=" + (a*table) + "<br>");
+//}
+
+
+//Q6.
+
+
+
+//Q7.
+//var heading = "<h1>Shopping Cart </h1>"
+//var list1= +prompt(" price of item 1 ");
+
+
+//document.write("Price of item 1 is :"+" "+list1);
+
+//var list2 = +prompt(" price of item 2 ");
+//document.write("Price of item 2 is :"+" "+list2);
+
+//var list3 = +prompt(" Order quantity of item 1 ");
+//document.write("Order quantity of item 1 is :"+" "+list3);
+
+//var list4 = +prompt("  Order quantity of item 1 ");
+//document.write("Order quantity of item 2 is :"+" "+list4);
+
+//var list5 = +prompt(" Shipping Charges ");
+//document.write("Order quantity of item 1 is :"+" "+list5);
+
+//var totalcost = +prompt("list1 + list2 + list3 + list4 + list5");
+//document.write("The total cost of your order is "+" "+totalcost);
+
+//Q8.
+//var totalmarks =980;
+//document.write("Total Marks:"+" "+tatalmarks);
+//var marksobtained=804;
+//document.write(" Marks Obtained:"+" "+marksobtained);
+//var a=totalmarks / marksobtained;
+//var b=a*100;
+//document.write(b);
+
+
+
+
+//Q10.
+//var a=8;
+//var b=2;
+//var c=a+b;
+//var d=c/2;
+//var e=d+5;
+//var f=e*10;
+//var g=f/2;
+
+
+//Q11.
+//document.write("The answer is :" + " "+ g);
+
+
+//var currentYear = +prompt("Enter your current year");
+//document.write("Your currentYear is:" + " " + currentYear);
+
+
+//var birthYear = +prompt("Enter your birth year");
+//document.write("Your birthYear  is:" + " " +  birthYear);
+
+
+//var age = +prompt("Enter your age");
+//document.write("Your age is:" + " " + age);
+
+
+
+//Q12.(a)
+//var r=5;
+//var pie=3.142;
+//var circumferenceOfCircle = 2*pie*r;
+//document.write("The  circumference Of Circle is :" + " "+ circumferenceOfCircle );
+
+//Q12.(b)
+//var r=8;
+//var pie=3.142;
+//var areaOfCircle = pie*r*r;
+//document.write("The  Area Of Circle is :" + " "+ areaOfCircle );
+
+
+//Q13.
+//var snack = "chocolate";
+//var age=15;
+//var maxAge=65;
+//var numPerDay=3;
+//var totalNeeded=(365*numPerDay)  * (maxAge - age);
+//var message = "You will need " + totalNeeded + " " + snack + "to last you will the ripe old age of" +  maxAge;
+//document.write(message);
+
+
+
+
+//CHAPTER # (6-9)
+//Q1.
+//var a=10;
+//var b=++a;
+//var c=b;
+//var d=c++;
+//var message = "The value of a is :" + " " + 10 + "<br>" + "---------------------"  + "<br>" + "<br>" + "The value of ++a is :" + " " + b + "<br>"
+//+ "Now the value of a is : " + " " + b+ "<br>" + "<br>" + "The value of ++a is :" + " " + d + "<br>"
+//+ "Now the value of a is : " + " " + c;
+//document.write(message);
+
+
+//Q3.
+//var userName= +prompt ("Enter your name");
+//var message = "HELLO WORLD!"
+//document.write(message);
